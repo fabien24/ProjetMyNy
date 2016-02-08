51817fc97d6b810dabe4bbd6af8898400db93e54
Ialacci <renaud.ialacci@gmail.com>
2016-02-08 16:32:55 +0100

    style.css +  loginhtml

--- php/loginhtml.php
+++ php/loginhtml.php
@@ -1,18 +1,20 @@
-<form action="" method="post">
-    <input type="email" name="email" placeholder="E-mail" ><br />
-    <input type="password" name="password" placeholder="Password"><br />
-    <input type="submit" value="Log in">
-    <a href="">Lost password</a><br />
-    <label>
-        <?php
-        if (!$emailFilled) {
-        ?> Email empty <?php
-        } elseif (!$pwFilled) {
-        ?> Password empty <?php
-        } elseif (!$emailVerif) {
-        ?> Invalid mail adrresse <?php
-        } elseif (!$pwVerif) {
-        ?> Password incorrect <?php
-        } ?>
-    </label>
-</form>
+<div id="loginScreen">  
+    <form action="" method="post">
+        <input type="email" name="email" placeholder="E-mail" ><br />
+        <input type="password" name="password" placeholder="Password"><br />
+        <input class="submit" type="submit" value="Log in">
+        <a href="">Lost password</a><br />
+        <label>
+            <?php
+            if (!$emailFilled) {
+            ?> Email empty <?php
+            } elseif (!$pwFilled) {
+            ?> Password empty <?php
+            } elseif (!$emailVerif) {
+            ?> Invalid mail adrresse <?php
+            } elseif (!$pwVerif) {
+            ?> Password incorrect <?php
+            } ?>
+        </label>
+    </form>
+</div>
