84736c48718f5a13667a259f0ae6768bade492d7
Ialacci <renaud.ialacci@gmail.com>
2016-02-08 10:31:06 +0100

    css, header, footer, index

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8"/>
-		<title>MyNy</title>
-	</head>
-	<body>
-		<h1>MyYelp</h1>
-		<h2>NOT F***N YOURS!</h2>
-	</body>
-</html>
+<?php
+	require_once 'php/header.php';
+?>
+
+			<!-- Google Map api -->
+			<section id="map">
+				
+			</section>
+
+			<!-- Slider .hidden on mobile devices -->
+			<section id="slider">
+				
+			</section>
+			<!-- locations list 2 per row (1 on mobile device) -->
+			<section id="locations">
+				
+			</section>
+
+<?php
+	require_once 'php/footer.php';
+?>
