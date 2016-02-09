ea0f609d14c0e9b90958a3a18b717971920e5925
fabien24 <fabien@Fabien-MacBook.local>
2016-02-09 16:16:44 +0100

    dot add

--- php/header.php
+++ php/header.php
@@ -1,6 +1,13 @@
 <?php 
 $account = isset($_SESSION['email']) ? $_SESSION['email'] : '' ;
-
+$sessRole = isset($_SESSION['role']) ? $_SESSION['role'] : '' ;
+if ($sessRole == 4) {
+	$role = 'admin';
+} elseif ($sessRole < 4 && !empty($sessRole)) {
+	$role = 'user';
+}else{
+	$role = '';
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,7 +30,7 @@ $account = isset($_SESSION['email']) ? $_SESSION['email'] : '' ;
 				<!-- shows who is logged in -->
 				<div id="login">
 					<ul>
-						<li>dot</li>
+						<li class="<?php echo $role ?>">dot</li>
 						<li><?php echo $account ;?></li>
 					</ul>
 				</div>
