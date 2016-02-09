b7eb8287f4f45e1031fb6a264c67889dd2ec62b8
fabien24 <fabien@Fabien-MacBook.local>
2016-02-09 16:49:52 +0100

    meta viewport

--- php/header.php
+++ php/header.php
@@ -15,6 +15,7 @@ if ($sessRole == 4) {
 <html>
 	<head>
 		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>MyNy</title>
 		<link href="css/style.css" rel="stylesheet"/>
 		<!-- Define Max and min width for devices -->
